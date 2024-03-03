1799fa2696dcb632b4538ee0cdc0fd025d69bb8f
Richard Nunes <richardl1010@outlook.com>
2024-03-03 19:56:35 -0300

    World - Structure done

--- src/world.py
+++ src/world.py
@@ -1,3 +1,40 @@
 """Implements the World for the simulation"""
-# TODO: Implement a logic to represent a space inside the World and it's status
-# TODO: Create the World class
+from dataclasses import dataclass
+from enum import Enum
+from functools import reduce
+from random import choice
+
+from utils.dimentions import Dimention
+
+
+class LocalStatus(Enum):
+    DIRTY = 0
+    CLEAN = 1
+
+
+class Local:
+    coord: Dimention
+    status: LocalStatus
+
+    def __init__(self, dim: Dimention | tuple[int, int], status: LocalStatus = LocalStatus.CLEAN) -> None:
+        self.coord = Dimention(*dim)
+        self.status = status
+
+    def __str__(self) -> str:
+        if self.status == LocalStatus.CLEAN:
+            return ' '
+        return "*"
+
+
+class World:
+    _map: list[Local]
+    _dim: Dimention
+
+    def __init__(self, dim: Dimention | tuple[int, int], local_size: int) -> None:
+        self._dim = dim if type(dim) == Dimention else Dimention(*dim)
+        self._map = [Local((0, x), LocalStatus(choice([0, 1])))
+                     for x in range(0, self._dim.x, local_size)]
+
+    def __str__(self) -> str:
+
+        return reduce(lambda ant, val: ant + str(val) + "|", self._map, '|')
